214099ae073206454d5db1a147aecd3111e0d2f6
sohanlakra1980-sys <sohanlakra1980@gmail.com>
2026-09-01 21:55:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BUDGET_CONFIG = {
   // Paste your deployed Google Apps Script Web App URL here.
-  webAppUrl: "https://script.google.com/macros/s/AKfycby4yQMhjNySZeoYeFvFYGrn0G6Qv7pV1QakSvFA8BBzdUOOZ1UzdfLDHxknfuthjrQ/exec",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbxrPrD1j720ekKoX4gOHsz9i9hrut70Ln5Fwlp3b9bX8-el9JPsOWMFAaeDbb1qB0E/exec",
   categories: [
     "Loans/EMI","Credit Card","Electricity & Water","Mobile/Internet",
     "Groceries","School Fees","Fuel/Transport","Insurance","Medical/Health",
