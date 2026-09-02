63317f5580592f8d762bd35368169ee6aa4e7539
sohanlakra1980-sys <sohanlakra1980@gmail.com>
2026-09-02 20:23:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-window.BUDGET_CONFIG = {
-  webAppUrl: "https://script.google.com/macros/s/AKfycbxrPrD1j720ekKoX4gOHsz9i9hrut70Ln5Fwlp3b9bX8-el9JPsOWMFAaeDbb1qB0E/exec",
-  categories: [
-    "Loans/EMI","Credit Card","Electricity & Water","Mobile/Internet",
-    "Groceries","School Fees","Fuel/Transport","Insurance","Medical/Health",
-    "Household","Shopping","Subscriptions","Savings/Investments","Miscellaneous"
-  ]
-};
+window.BUDGET_CONFIG={webAppUrl:"https://script.google.com/macros/s/AKfycbxrPrD1j720ekKoX4gOHsz9i9hrut70Ln5Fwlp3b9bX8-el9JPsOWMFAaeDbb1qB0E/exec",categories:["Loans/EMI","Credit Card","Electricity & Water","Mobile/Internet","Groceries","School Fees","Fuel/Transport","Insurance","Medical/Health","Household","Shopping","Subscriptions","Savings/Investments","Miscellaneous"]};
