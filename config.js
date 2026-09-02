f3e3ff7ae6e8e9c147611dbdb28a36da359368f9
sohanlakra1980-sys <sohanlakra1980@gmail.com>
2026-09-02 17:05:27 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.BUDGET_CONFIG = {
-  // Paste your deployed Google Apps Script Web App URL here.
   webAppUrl: "https://script.google.com/macros/s/AKfycbxrPrD1j720ekKoX4gOHsz9i9hrut70Ln5Fwlp3b9bX8-el9JPsOWMFAaeDbb1qB0E/exec",
   categories: [
     "Loans/EMI","Credit Card","Electricity & Water","Mobile/Internet",
